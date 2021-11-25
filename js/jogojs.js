7116452c1ba290485bbaa086f7c1afd8fca92bc7
Vitor Silva <vitor.petro2@gmail.com>
2021-11-24 22:28:06 -0300

    subindo arquivos

---
+++ js/jogojs.js
@@ -0,0 +1,46 @@
+function facaSuaEscolhaSalahadin (){
+    
+
+        var opcao = prompt ('1- Entrevistar as pessoas no aeroporto e verificar as câmeras de segurança \n 2- Ir até o bar para refazer o caminho das vítimas')
+
+        if (opcao == 1){
+                window.location.href='../views/salahadin2.html'
+            
+    }
+        else if (opcao == 2){
+            window.location.href='../views/salahadin3.html'
+
+        }
+}
+
+function facaSuaEscolhaSteve (){
+    
+
+    var opcao = prompt ('1- Seguir as pegadas que mais parece ranhuras causadas por garras afiadas no chão de terra \n 2- Seguir as marcas de sangue que parecem um pouco mais antigas e não parece ser do casal, mas podem ser de possíveis vítmias anteriores.')
+
+    if (opcao == 1){
+            window.location.href='../views/steve1.html'
+        
+}
+    else if (opcao == 2){
+        window.location.href='../views/steve2.html'
+
+    }
+}
+
+function facaSuaEscolhaLavinia (){
+    
+
+    var opcao = prompt ('1- Ir até o laborário que fica uns 100 km do local onde ela se encontra. \n 2- Se armar até os "dentes" e partir para a floresta')
+
+    if (opcao == 1){
+            window.location.href='../views/lavinia1.html'
+        
+}
+    else if (opcao == 2){
+        window.location.href='../views/lavinia2.html'
+
+    }
+}
+
+
